5385ebd1f6833a200b3492e7b2c9d9f50f8d37d8
Atugatran <atugatran@gmail.com>
2022-11-26 00:50:15 +0530

    Add deploy

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import { useGlobalContext } from "./context";
-import { NavLink } from "react-router-dom";
+// import { NavLink } from "react-router-dom";
 import { Button } from "./styles/Button";
 
 const Projects = () => {
