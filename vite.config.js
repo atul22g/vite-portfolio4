ce1faae63a86e00464662112973b71968b68bd92
Atugatran <atugatran@gmail.com>
2022-11-26 02:04:33 +0530

    Add main deploy

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://atugatran-projects.github.io/React-portfolio4/",
+  base: "/React-portfolio4/",
   plugins: [react()],
 });
